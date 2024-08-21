040d5bbc8d783c93360c5a9ecae3a73efa0c110c
Etin Obaseki <etinosaobaseki@gmail.com>
2024-08-21 19:55:44 +0100

    feat(api): add routes for companies

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -4,9 +4,13 @@ import "net/http"
 
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/v1/healthcheck", app.healthcheck)
+
 	mux.HandleFunc("/v1/roles", app.getCreateRolesHandler)
 	mux.HandleFunc("/v1/roles/", app.getUpdateDeleteRolesHandler)
+
+	mux.HandleFunc("/v1/companies", app.getCreateCompaniesHandler)
 
 	return mux
 }
